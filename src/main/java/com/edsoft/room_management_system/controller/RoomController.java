2a5ae39c680e21b9485b085d1b54acde87b7aa78
erdincdagli35 <3rd1fbslash@gmail.com>
2025-11-28 00:22:50 +0300

    CrossOrigin has been added

--- src/main/java/com/edsoft/room_management_system/controller/RoomController.java
+++ src/main/java/com/edsoft/room_management_system/controller/RoomController.java
@@ -2,6 +2,7 @@ package com.edsoft.room_management_system.controller;
 
 import com.edsoft.room_management_system.model.Room;
 import com.edsoft.room_management_system.service.RoomService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
+@CrossOrigin({"https://roomservice.up.railway.app", "http://localhost:8080"})
 public class RoomController {
 
     private final RoomService roomService;
